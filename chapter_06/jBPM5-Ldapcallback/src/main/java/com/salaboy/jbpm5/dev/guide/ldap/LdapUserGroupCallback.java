f7e9e406fbdd00c64148d6a591fd8f4e8dc49119
unknown <dcalcaprina@wks0076.viridian.local>
2012-01-30 15:26:29 -0300

    Made simple test which shows a jbpm5 callback interface using an embeeded ldap server.

---
+++ chapter_06/jBPM5-Ldapcallback/src/main/java/com/salaboy/jbpm5/dev/guide/ldap/LdapUserGroupCallback.java
@@ -0,0 +1,48 @@
+package com.salaboy.jbpm5.dev.guide.ldap;
+
+import java.util.List;
+
+import org.jbpm.task.service.UserGroupCallback;
+
+/**
+ * Implementation of {@link UserGroupCallback}, which interacts with ldap to get
+ * information about users, groups and its relationship.
+ * 
+ * @author calcacuervo
+ * 
+ */
+public class LdapUserGroupCallback implements UserGroupCallback {
+
+	private LdapQueryHelper query;
+
+	public void setQuery(LdapQueryHelper query) {
+		this.query = query;
+	}
+
+	@Override
+	public boolean existsGroup(String group) {
+		return query.existsGroup(group);
+	}
+
+	@Override
+	public boolean existsUser(String user) {
+		return query.existsUser(user);
+	}
+
+	@Override
+	public List<String> getGroupsForUser(String user) {
+		return query.groupsForUser(user);
+
+	}
+
+	@Override
+	public List<String> getGroupsForUser(String arg0, List<String> arg1) {
+		return query.groupsForUser(arg0);
+	}
+
+	@Override
+	public List<String> getGroupsForUser(String arg0, List<String> arg1,
+			List<String> arg2) {
+		return query.groupsForUser(arg0);
+	}
+}
