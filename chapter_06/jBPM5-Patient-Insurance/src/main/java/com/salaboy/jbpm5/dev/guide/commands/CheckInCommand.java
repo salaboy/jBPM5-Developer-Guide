5a23139272452819b6af5209b420a270eee589c5
Esteban Aliverti <esteban.aliverti@gmail.com>
2012-09-29 19:27:08 +0200

    project renamed

--- chapter_06/jBPM5-Patient-Insurance/src/main/java/com/salaboy/jbpm5/dev/guide/commands/CheckInCommand.java
+++ chapter_06/jBPM5-Patient-Insurance/src/main/java/com/salaboy/jbpm5/dev/guide/commands/CheckInCommand.java
@@ -1,5 +1,6 @@
 package com.salaboy.jbpm5.dev.guide.commands;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import javax.inject.Named;
 import org.jbpm.executor.api.Command;
 import org.jbpm.executor.api.CommandContext;
@@ -8,21 +9,21 @@ import org.jbpm.executor.api.ExecutionResults;
 @Named
 public class CheckInCommand implements Command {
 
-	private static int checkInCount = 0;
+	private static AtomicInteger checkInCount = new AtomicInteger();
 	
 	public ExecutionResults execute(CommandContext ctx) {
 		String patientName = (String) ctx.getData("bedrequest_patientname");
 		System.out.println("Check In for patient " + patientName + " happening NOW!!!");
-		checkInCount++;
+		checkInCount.incrementAndGet();
 		return null;
 	}
 	
 	public static int getCheckInCount() {
-		return checkInCount;
+		return checkInCount.get();
 	}
 
 	public static void reset() {
-		checkInCount = 0;
+		checkInCount.set(0);
 	}
 	
 }
