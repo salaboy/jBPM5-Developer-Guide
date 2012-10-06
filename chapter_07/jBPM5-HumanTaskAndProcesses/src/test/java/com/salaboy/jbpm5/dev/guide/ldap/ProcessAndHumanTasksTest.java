eed273cb3931ab5ab79aedb7dd585c63107caf0c
salaboy <salaboy@gmail.com>
2012-10-06 19:08:12 -0300

    - Updating Process & HT interaction + Identity configuration Examples

--- chapter_07/jBPM5-HumanTaskAndProcesses/src/test/java/com/salaboy/jbpm5/dev/guide/ldap/ProcessAndHumanTasksTest.java
+++ chapter_07/jBPM5-HumanTaskAndProcesses/src/test/java/com/salaboy/jbpm5/dev/guide/ldap/ProcessAndHumanTasksTest.java
@@ -15,10 +15,11 @@ import org.drools.builder.KnowledgeBuilderErrors;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
 import org.drools.io.impl.ClassPathResource;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.process.ProcessInstance;
 import org.jbpm.process.workitem.wsht.GenericHTWorkItemHandler;
 import org.jbpm.process.workitem.wsht.LocalHTWorkItemHandler;
-import org.jbpm.process.workitem.wsht.SyncWSHumanTaskHandler;
 import org.jbpm.task.identity.UserGroupCallbackManager;
 import org.jbpm.task.query.TaskSummary;
 import org.jbpm.task.service.TaskService;
@@ -26,11 +27,12 @@ import org.jbpm.task.service.local.LocalTaskService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
 import org.springframework.security.ldap.server.ApacheDSContainer;
 
-public class LdapUerGroupCallbackTest {
+public class ProcessAndHumanTasksTest {
 
     private ApacheDSContainer container;
     private LdapQueryHelper ldapQuery;
@@ -38,10 +40,10 @@ public class LdapUerGroupCallbackTest {
 
     @Before
     public void setUp() throws Exception {
-        container = new ApacheDSContainer("o=mojo", "classpath:test.ldif");
+        container = new ApacheDSContainer("o=mojo", "classpath:identity-repository.ldif");
         container.setPort(9898);
         container.afterPropertiesSet();
-
+        
         LdapContextSource cs = new LdapContextSource();
         cs.setUrl("ldap://localhost:9898/");
         cs.setBase("o=mojo");
@@ -53,7 +55,7 @@ public class LdapUerGroupCallbackTest {
         ldapQuery = new LdapQueryHelper(ldapTemplate);
 
         // By Setting the jbpm.usergroup.callback property with the call
-        // back class full name, task server will use this to validate the
+        // back class full name, task service will use this to validate the
         // user/group exists and its permissions are ok.
         System.setProperty("jbpm.usergroup.callback",
                 "com.salaboy.jbpm5.dev.guide.ldap.LdapUserGroupCallback");
@@ -62,28 +64,37 @@ public class LdapUerGroupCallbackTest {
     }
 
     @Test
-    public void testLdapCallback() throws InterruptedException {
-        Assert.assertEquals(true, this.ldapQuery.existsUser("calcacuervo"));
-        Assert.assertEquals(false, this.ldapQuery.existsUser("calcacuervo2"));
-        System.out.println("Hi!");
+    public void testUsers(){
+        assertEquals(true, this.ldapQuery.existsUser("calcacuervo"));
+        assertEquals(false, this.ldapQuery.existsUser("calcacuervo2"));
+        assertEquals(true, this.ldapQuery.existsUser("salaboy"));
+        assertEquals(true, this.ldapQuery.existsUser("esteban"));
+    }
+    
+    @Test
+    public void testProcessWithHumanTasks() throws InterruptedException {
 
         StatefulKnowledgeSession ksession = this.initializeSession();
+        
+        KnowledgeRuntimeLoggerFactory.newConsoleLogger(ksession);
+        
         GenericHTWorkItemHandler htHandler = this.createTaskHandler(ksession);
         ksession.getWorkItemManager().registerWorkItemHandler("Human Task",
                 htHandler);
 
         Map<String, Object> initialParams = new HashMap<String, Object>();
         initialParams.put("actor", "calcacuervo");
-        ksession.startProcess("chapter_06_simple_review", initialParams);
-
+        ProcessInstance processInstance = ksession.startProcess("chapter_07_simple_review", initialParams);
+        assertEquals(ProcessInstance.STATE_ACTIVE, processInstance.getState());
         // first, calcacuervo will make its own review..
         List<TaskSummary> tasks = this.service.getTasksAssignedAsPotentialOwner("calcacuervo", "en-UK");
-        Assert.assertEquals(1, tasks.size());
+        assertEquals(1, tasks.size());
 
         this.service.start(tasks.get(0).getId(), "calcacuervo");
+        
         this.service.complete(tasks.get(0).getId(), "calcacuervo", null);
 
-        Thread.sleep(2000);
+        
 
         // now, a user with role TL will see the task.. esteban is one of them
         List<TaskSummary> estebanTasks = this.service.getTasksAssignedAsPotentialOwner("esteban",
@@ -93,7 +104,7 @@ public class LdapUerGroupCallbackTest {
         this.service.start(estebanTasks.get(0).getId(), "esteban");
         this.service.complete(estebanTasks.get(0).getId(), "esteban", null);
 
-        Thread.sleep(2000);
+        
 
         // now, a user with role HR will see the task.. mariano is one of them
         List<TaskSummary> marianoTasks = this.service.getTasksAssignedAsPotentialOwner("mariano",
@@ -103,6 +114,8 @@ public class LdapUerGroupCallbackTest {
         this.service.start(marianoTasks.get(0).getId(), "mariano");
         this.service.complete(marianoTasks.get(0).getId(), "mariano", null);
 
+        assertEquals(ProcessInstance.STATE_COMPLETED, processInstance.getState());
+        
     }
 
     @After
@@ -139,7 +152,7 @@ public class LdapUerGroupCallbackTest {
         LocalTaskService taskService = new LocalTaskService(ts);
         LocalHTWorkItemHandler taskHandler = new LocalHTWorkItemHandler(
                 taskService, ksession);
-        taskHandler.connect();
+//        taskHandler.connect();
         this.service = taskService;
         return taskHandler;
     }
