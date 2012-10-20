ec8d5f44af862ec0e76347db80a2ef4f5ee3a8a9
salaboy <salaboy@gmail.com>
2012-10-20 11:00:05 -0300

    - Exclusive gateways - RuleAwareProcessEventListener Tests initial

---
+++ chapter_09/jBPM5-Process-Rules-Patterns/src/test/java/com/salaboy/jbpm5/evolution/RuleAwareProcessListenerTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.salaboy.jbpm5.evolution;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author salaboy
+ */
+public class RuleAwareProcessListenerTest {
+    
+    public RuleAwareProcessListenerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
